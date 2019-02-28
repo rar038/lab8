e78e7153c7edfefa65ac1074e2a172f4e9f2d26b
Rachel Reyes <rar038@ucsd.edu>
2019-02-28 10:49:27 -0800

    thirdpartyAPI lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,8 @@
 function initCamera() {
   // Uncomment and fill in the correct selectors below.
-  // capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+    capture($('#camera-video'),
+          $('#camera-canvas'),
+          $('#camera-button'));
 
   // For Reference:
   // capture($('#camera-video'),
@@ -20,7 +20,7 @@ function capture(video, canvas, snapshotButton) {
   var successCallback = function(mediaStream) {
     //The success callback function. On user click of snapshot button,
     //draw the image on the canvas.
-    // video.attr('src', window.URL.createObjectURL(mediaStream));
+    //video.attr('src', window.URL.createObjectURL(mediaStream));
     var vid = document.querySelector('#camera-video');
     vid.srcObject = mediaStream;
     snapshotButton.click(function(e) {
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'soifxuWZMGjQVJuLpY00rGVctZ6e1thI';
+
+	// 'map' referes to a <div> element with the id map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
